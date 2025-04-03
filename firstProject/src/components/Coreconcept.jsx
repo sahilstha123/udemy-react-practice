28d5dccfff13837a43960ebe4ee0aa08fdb0d83f
sahilstha123 <sthasahil401@gmail.com>
2025-04-03 20:42:32 +0545

    added title and desc in li

--- firstProject/src/components/Coreconcept.jsx
+++ firstProject/src/components/Coreconcept.jsx
@@ -1,24 +1,27 @@
 import React from "react";
 
-const Coreconcept = ({ title, image }) => {
+const Coreconcept = ({ title, image, desc }) => {
+  console.log("desc:", desc);
+
   return (
-    <div className="flex-1 p-4 group"> {/* Parent with 'group' */}
-      <li className="w-full max-w-xs bg-gray-900 rounded-lg shadow-lg transition-all cursor-grab group-hover:scale-[0.85] hover:!scale-100 hover:shadow-2xl h-72 sm:h-full group-hover:blur-sm hover:!blur-none">
+    <div className="p-2 flex justify-center">
+      <li className="w-full max-w-[420px] sm:max-w-sm md:max-w-md lg:max-w-lg bg-gray-900 rounded-lg shadow-lg transition-all cursor-pointer group-hover:blur-sm hover:!blur-none group-hover:scale-90
+        hover:scale-100 hover:shadow-2xl h-auto sm:h-full flex flex-col items-center group p-4 ">
+        
         {/* Image */}
         <img
           src={image}
           alt={title}
-          className="w-40 sm:w-52 mx-auto object-cover md:w-48 h-40 sm:h-48"
+          className="w-32 sm:w-40 md:w-48 lg:w-56 h-32 sm:h-40 md:h-48 lg:h-56 object-cover rounded-lg mt-4"
         />
 
         {/* Card Content */}
-        <div className="px-4 py-2">
-          <h3 className="text-xl sm:text-2xl text-center font-semibold text-white">
+        <div className="px-4 py-5 w-full text-center">
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-bold text-white">
             {title}
           </h3>
-          <p className="mt-2 text-balance text-gray-300 lg:tracking-wider text-justify">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-            maiores reprehenderit.
+          <p className="mt-3 text-gray-300 text-base sm:text-lg  leading-relaxed">
+            {desc}
           </p>
         </div>
       </li>
