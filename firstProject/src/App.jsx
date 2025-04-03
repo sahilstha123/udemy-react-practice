6c56269c27d048b5da70dee850205fa4124f9230
sahilstha123 <sthasahil401@gmail.com>
2025-04-03 20:41:54 +0545

    update app

--- firstProject/src/App.jsx
+++ firstProject/src/App.jsx
@@ -1,18 +1,20 @@
 import "./app.css";
 import Coreconcept from "./components/Coreconcept";
 import Header from "./components/Header";
-import componentImg from "./assets/components.png";
-
+// import componentImg from "./assets/components.png";
+import {CORE_CONCEPTS as Core}from "./components/data";
 function App() {
   return (
     <div className="container mx-auto min-h-screen flex flex-col items-center p-4 w-full hides">
       <Header />
-      <main className="mt-8 w-[90%] sm:w-[80%] lg:w-[70%] bg-black min-h-full p-4 rounded-lg">
+      <main className="mt-8 w-[97%] sm:w-[90%] md:w-[75%] bg-black min-h-full p-4 rounded-lg">
         <h2 className="text-center mt-4 text-4xl text-white font-bold">Core Concept</h2>
-        <ul className="flex flex-wrap gap-5 justify-center group">
-          <Coreconcept title="Component 1" image={componentImg} />
-          <Coreconcept title="Component 2" image={componentImg} />
-          <Coreconcept title="Component 3" image={componentImg} />
+        <ul className="flex flex-wrap lg:flex-nowrap gap-3 justify-center group mt-3">
+         {Core.map((item,index)=>{
+          return(
+            <Coreconcept title={item.title} image = {item.image} key={index} desc={item.description}/>
+          )
+         })}
         </ul>
       </main>
     </div>
