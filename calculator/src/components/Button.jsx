cdebd2be10a15b80d589c5cfa46acef6fb5ee69b
sahilstha123 <sthasahil401@gmail.com>
2025-04-07 16:24:07 +0545

    update button file for scaling

--- calculator/src/components/Button.jsx
+++ calculator/src/components/Button.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
-const Button = ({ label, className = "",handleOnButtonClick }) => {
+const Button = ({ label, className = "", handleOnButtonClick, handleOnMousedown, scaling, isMousedown }) => {
   const buttonStyles = `text-white text-2xl sm:text-3xl font-bold h-20 sm:h-24 sm:rounded-full shadow-md shadow-emerald-100 cursor-pointer transition-all duration-200 hover:scale-105 ${className}`;
-  
-  return <button className={buttonStyles} onClick={()=>handleOnButtonClick(label)}>{label}</button>;
+
+  // Conditionally apply the scaling transform if the button is being pressed
+  const scale = isMousedown === label ? scaling : null;
+
+  return (
+    <button
+      className={buttonStyles}
+      style={scale} // Apply the transform styles here
+      onClick={() => handleOnButtonClick(label)}
+      onMouseDown={() => handleOnMousedown(label)}
+    >
+      {label}
+    </button>
+  );
 };
+
 
 export default Button;
